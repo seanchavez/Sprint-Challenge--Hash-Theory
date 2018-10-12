827e9b47f22dac643dc73306b402c782ab64630d
sean chavez <seanchavez@seanchavezsMBP.home>
2018-10-12 09:07:00 -0700

    Sean Chavez--Sprint-Challenge--Hash-Theory Start on ex1.py

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,5 +1,7 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+    weight_table = {}
+    for i in range(len(weights)):
+      weight_table[str(weights[i])] = i
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
