e28b4306da1967227bc9520ad0032c151b503767
sean chavez <seanchavez@seanchavezsMBP.home>
2018-10-12 11:05:48 -0700

    Complete ex2.py

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,16 @@
 def reconstruct_trip(tickets):
-  pass 
+    ticket_table = {}
+    trip = []
+
+    for ticket in tickets:
+        ticket_table[ticket[0]] = ticket[1]
+    current_ticket = ticket_table[None]
+    while current_ticket:
+        trip.append(current_ticket)
+        if current_ticket not in ticket_table:
+            return []
+        current_ticket = ticket_table[current_ticket]   
+    return trip 
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
