50320e6968dea12f16a9c6110d5a8fc8f9806f6b
sean chavez <seanchavez@seanchavezsMBP.home>
2018-10-12 10:32:39 -0700

    Complete ex1.py

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,8 +1,11 @@
 def get_indices_of_item_weights(weights, limit):
     weight_table = {}
     for i in range(len(weights)):
-      weight_table[str(weights[i])] = i
-
+        if weights[i] not in weight_table:
+            weight_table[weights[i]] = i
+        if limit - weights[i] in weight_table and weight_table[limit - weights[i]] != i:
+            return (i, weight_table[limit - weights[i]])
+    return ()
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
   pass 
